a426691c23a9802cc2b1ff0ce8dc2d2cae5922b4
takasumi-m <takasumi.mishima@gmail.com>
2024-10-10 09:54:52 +0900

    update: add errormessage div

--- src/components/forms/Caption.jsx
+++ src/components/forms/Caption.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import './Caption.css';
 
 const CaptionComponent = ({ value, onChange }) => {
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value;
+        const byteLength = new Blob([inputValue]).size;
+
+        if (byteLength > 65536) { // 64KB = 65536 bytes
+            setErrorMessage("入力できるのは64KBまでです");
+            return;
+        }
+
+        setErrorMessage("");
+        onChange(inputValue);
+    };
+
     return (
-        <div className="caption-container">
-            <label htmlFor="caption">Caption</label>
-            <input type="text" id="caption" name="caption" value={value} onChange={(e) => onChange(e.target.value)}/>
-        </div>
+        <>
+            <div className="caption-container">
+                <div className="caption-container-input">
+                    <label htmlFor="caption">Caption</label>
+                    <textarea id="caption" name="caption" value={value} onChange={handleChange}/>
+                </div>
+                {errorMessage && (
+                    <div className='caption-container-error-message'>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
